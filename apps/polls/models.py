db9b8e27901c8b1d982967d953f70b8bd45e5029
Nelson Guillermo <nelsonguillerm7@gmail.com>
2023-02-19 00:26:44 -0500

    ADD: Class Meta models.

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -8,6 +8,10 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    class Meta:
+        verbose_name = "Question"
+        verbose_name_plural = "Questions"
+
     def __str__(self):
         return self.question_text
 
@@ -24,6 +28,10 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Choice"
+        verbose_name_plural = "Choices"
 
     def __str__(self):
         return self.choice_text
